f3dd3063afb3f4d6371d4b057fa85c81f0f6ace2
naumanshahid007 <hwhashmi1625@gmail.com>
2018-06-22 17:35:44 +0500

    game form modified

--- backend/views/game/_form.php
+++ backend/views/game/_form.php
@@ -1,45 +1,83 @@
-<?php
+    <?php
 
-use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+    use yii\helpers\Html;
+    use yii\widgets\ActiveForm;
 
-/* @var $this yii\web\View */
-/* @var $model backend\models\Game */
-/* @var $form yii\widgets\ActiveForm */
-?>
+    /* @var $this yii\web\View */
+    /* @var $model backend\models\Game */
+    /* @var $form yii\widgets\ActiveForm */
+    ?>
 
-<div class="game-form">
+    <div class="game-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+        <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'gt_id')->textInput() ?>
+        <!-- row 1 start here -->
+            <div class="row">
+                <div class="col-md-4">
+                    <?= $form->field($model, 'gt_id')->textInput() ?>
+                </div>
+                <div class="col-md-4">
+                    <?= $form->field($model, 'g_start_datetime')->textInput() ?>
+                </div>
+                <div class="col-md-4">
+                  <?= $form->field($model, 'g_end_datetime')->textInput() ?>  
+                </div>
+            </div>
+        <!-- row 1 close here -->
 
-    <?= $form->field($model, 'g_start_datetime')->textInput() ?>
+        <!-- row 2 start here -->
+            <div class="row">
+                <div class="col-md-4">
+                    <?= $form->field($model, 'player1_name')->textInput(['maxlength' => true]) ?>
+                </div>
+                <div class="col-md-4">
+                   <?= $form->field($model, 'player2_name')->textInput() ?> 
+                </div>
+                <div class="col-md-4">
+                     <?= $form->field($model, 'product_id')->textInput() ?>
+                </div>
+            </div>
+        <!-- row 2 close here -->
 
-    <?= $form->field($model, 'g_end_datetime')->textInput() ?>
+        <!-- row 3 start here -->
+            <div class="row">
+                <div class="col-md-4">
+                    <?= $form->field($model, 'product_qty')->textInput() ?>
+                </div>
+                <div class="col-md-4">
+                   <?= $form->field($model, 'product_t_price')->textInput() ?> 
+                </div>
+                <div class="col-md-4">
+                     <?= $form->field($model, 'created_at')->textInput() ?>
+                </div>
+            </div>
+        <!-- row 3 close here -->
 
-    <?= $form->field($model, 'player1_name')->textInput(['maxlength' => true]) ?>
+        <!-- row 4 start here -->
+            <div class="row">
+                <div class="col-md-4">
+                   <?= $form->field($model, 'created_by')->textInput() ?>
+                </div>
+                <div class="col-md-4">
+                    <?= $form->field($model, 'updated_at')->textInput() ?>
+                </div>
+                <div class="col-md-4">
+                   <?= $form->field($model, 'updated_by')->textInput() ?>  
+                </div>
+            </div>
+        <!-- row 4 close here -->
 
-    <?= $form->field($model, 'player2_name')->textInput() ?>
+        <!-- row 5 start here -->
+            <div class="row">
+                <div class="col-md-4">
+                   <div class="form-group">
+            <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+        </div>
+                </div>
+            </div>
+        <!-- row 5 close here -->
 
-    <?= $form->field($model, 'product_id')->textInput() ?>
+        <?php ActiveForm::end(); ?>
 
-    <?= $form->field($model, 'product_qty')->textInput() ?>
-
-    <?= $form->field($model, 'product_t_price')->textInput() ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'created_by')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_by')->textInput() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
-
-    <?php ActiveForm::end(); ?>
-
-</div>
