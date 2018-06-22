5bf5a72fe4282c962f5a801c130b2213079cc87a
naumanshahid007 <hwhashmi1625@gmail.com>
2018-06-22 17:15:21 +0500

    modified bills form

--- backend/views/bill/_form.php
+++ backend/views/bill/_form.php
@@ -1,35 +1,67 @@
-<?php
+    <?php
 
-use yii\helpers\Html;
-use yii\widgets\ActiveForm;
+    use yii\helpers\Html;
+    use yii\widgets\ActiveForm;
 
-/* @var $this yii\web\View */
-/* @var $model backend\models\Bill */
-/* @var $form yii\widgets\ActiveForm */
-?>
+    /* @var $this yii\web\View */
+    /* @var $model backend\models\Bill */
+    /* @var $form yii\widgets\ActiveForm */
+    ?>
 
-<div class="bill-form">
+    <div class="bill-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+        <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'player_id')->textInput() ?>
+        <div class="row">
+            <div class="col-md-4">
+                 <?= $form->field($model, 'player_id')->textInput() ?>
+            </div>
+                 
+            <div class="col-md-4">
+                 <?= $form->field($model, 'table_id')->textInput() ?>
+            </div>
 
-    <?= $form->field($model, 'table_id')->textInput() ?>
+            <div class="col-md-4">
+                 <?= $form->field($model, 'bill_datetime')->textInput() ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'bill_datetime')->textInput() ?>
+        <div class="row">
+            <div class="col-md-4">
+                 <?= $form->field($model, 'created_at')->textInput() ?>
+            </div>
+                 
+            <div class="col-md-4">
+                 <?= $form->field($model, 'created_by')->textInput() ?>
+            </div>
+                
+            <div class="col-md-4">
+                 <?= $form->field($model, 'updated_at')->textInput() ?>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
+        <div class="row">
+            <div class="col-md-4">
+                  <?= $form->field($model, 'updated_by')->textInput() ?>
+            </div>
+                 
+            <div class="col-md-4">
+                 
+            </div>
+                
+            <div class="col-md-4">
+                 
+            </div>
+        </div>
 
-    <?= $form->field($model, 'created_by')->textInput() ?>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="form-group">
+            <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+            </div>
+            </div>
+        </div>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+        <?php ActiveForm::end(); ?>
 
-    <?= $form->field($model, 'updated_by')->textInput() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
-
-    <?php ActiveForm::end(); ?>
-
-</div>
